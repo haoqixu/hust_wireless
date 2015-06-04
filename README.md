808a99cfa593ab4b8917dbad6471cba6ade00705
haoqixu <haoqixu@hustunique.com>
2015-06-04 12:04:55 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,17 @@
+HUST_WIRELESS Login Tool
+=========================
+This is a simple comand-line tool to login HUST\_WIRELESS **without a Web Browser**
+
+
+THIRD-PARTY DEPENDANCIES
+------------------------
+This simple tool is written in python using these librarys:
+
+* REQUESTS - Dealing with HTTP requests
+
+
+USAGE
+-----
+    hust_wireless.py
+
+    hust_wireless.py -u <username> -p <password>
---
+++ hust_wireless.py
@@ -0,0 +1,50 @@
+#!/bin/env python
+import argparse
+import getpass
+import requests
+import re
+
+
+parser = argparse.ArgumentParser(
+        description='a cli tool to login HUST_WIRELESS without Web Browsers')
+# options
+parser.add_argument(
+        '-u', '--username', metavar='username', help='specify a username')
+parser.add_argument(
+        '-p', '--password', metavar='password', help='specify a password')
+parser.add_argument(
+        '-q', '--quiet', action='store_true', help='do not print the result')
+args = parser.parse_args()
+
+
+result = requests.get('http://www.baidu.com')
+
+if result.text.find('eportal') != -1:
+    username = args.username if args.username else input('username: ')
+    password = args.password if args.password else getpass.getpass('password:')
+
+    pattarn = re.compile(r"href=.*?\?(.*?)'")
+    params = pattarn.findall(result.text)
+
+    url = (
+            'http://192.168.50.3:8080/eportal/userV2.do?'
+            'method=login&param=true&' + params[0])
+
+    post_data = {
+            'username': username,
+            'pwd': password
+            }
+
+    result = requests.request('POST', url, data=post_data)
+    if result.text.find('认证失败') != -1:
+        pattarn = re.compile(r"(认证失败.*?)(?:'|\")")
+        login_res = pattarn.findall(result.text)
+        print(login_res[0])
+    else:
+        print('Successfully Login In')
+
+elif result.text.find('baidu') != -1:
+    print('You are already logged in')
+
+else:
+    print("Opps, something goes wrong!")
