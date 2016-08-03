f6c648812fd3dd6e090c776434d3c92852a88f4c
xu0o0 <haoqixu@hustunique.com>
2016-08-04 01:29:31 +0800

    add .gitignore

--- hust_wireless.py
+++ hust_wireless.py
@@ -1,27 +1,44 @@
 #!/bin/env python
+# -*- coding: UTF-8 -*-
 import argparse
 import getpass
 import requests
+import sys
 import re
+# import keyword
 
 
-parser = argparse.ArgumentParser(
-        description='a cli tool to login in HUST_WIRELESS without Web Browsers')
-# options
-parser.add_argument(
-        '-u', '--username', metavar='username', help='specify a username')
-parser.add_argument(
-        '-p', '--password', metavar='password', help='specify a password')
-parser.add_argument(
-        '-q', '--quiet', action='store_true', help='do not print the result')
+description = 'Login in HUST_WIRELESS without web browsers'
+
+# a list of (args, kwargs)
+options = (
+    (('-u', '--username'), {'metavar': 'username'}),
+    (('-p', '--password'), {'metavar': 'password'}),
+    (('-q', '--quiet'), {
+        'action': 'store_true',
+        'help': "don't print to stdout"}),
+)
+
+parser = argparse.ArgumentParser(description=description)
+for opt in options:
+    parser.add_argument(*opt[0], **opt[1])
 args = parser.parse_args()
 
 
-result = requests.get('http://www.baidu.com')
+try:
+    result = requests.get('http://www.baidu.com')
+except Exception:
+    print('Failed to connecte test webside!')
+    sys.exit()
 
 if result.text.find('eportal') != -1:
-    username = args.username if args.username else input('username: ')
-    password = args.password if args.password else getpass.getpass('password:')
+
+    try:
+        input = raw_input
+    except NameError:
+        pass
+    username = args.username if args.username else input('Username: ')
+    password = args.password if args.password else getpass.getpass()
 
     pattarn = re.compile(r"href=.*?\?(.*?)'")
     params = pattarn.findall(result.text)
